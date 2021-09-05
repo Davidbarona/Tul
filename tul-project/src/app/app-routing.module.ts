c79da64fbaae3daa9b2e8879d00ee4bb1508544d
unknown <davidb@soymomo.com>
2021-09-04 19:12:26 -0500

    create forms

--- tul-project/src/app/app-routing.module.ts
+++ tul-project/src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { LoginComponent } from './pages/login/login.component';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: '/welcome' },
-  { path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.module').then(m => m.WelcomeModule) },
-  { path:'login',component:LoginComponent}
+ 
+  { path:'login', component:LoginComponent},
+  { path:'register',component:RegisterComponent}
 ];
 
 @NgModule({
